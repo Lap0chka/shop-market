55c29525928369c08a9996a8634e424bed22c914
Lap0chka <danya.tkachenko.1997@gmail.com>
2024-01-15 10:22:08 +0000

    15.01.2025PAW(1)

--- users/authentication.py
+++ users/authentication.py
@@ -1,8 +1,16 @@
 from users.models import Profile  # Импортируйте вашу модель Profile
+from users.models import EmailVerification, User
+import uuid
+from datetime import timedelta
+from django.utils.timezone import now
+
 
 def create_profile(backend, user, *args, **kwargs):
     """Создать профиль пользователя для социальной аутентификации """
-
     profile, created = Profile.objects.get_or_create(user=user)
+    if not user.is_verifacated_email:
+        expiration = now() + timedelta(hours=24)
+        record = EmailVerification.objects.create(code=uuid.uuid4(), user=user, expiration=expiration)
+        record.send_verifacation_email()
 
 
